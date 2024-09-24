41da02f128e2d38e34a2a819fafc8f7547ef4d8b
Leather-Face619 <rahul852369hhhhh@gmail.com>
2024-09-24 20:38:46 +0530

    3rd commit

--- script.js
+++ script.js
@@ -10,6 +10,33 @@ requestAnimationFrame(raf)
 
 document.querySelectorAll('.grid-item').forEach((elem)=>{
   var img = elem.querySelector("img")
-  console.log(img.src);
-  
+  let tl = gsap.timeline()
+
+  let xTransform = gsap.utils.random(-100,100);
+
+  tl.set(img,{
+    transformOrigin:`${xTransform < 0 ? 0 : "100%"}`,
+  },"a1")
+  .to(img,{
+    scale:0,
+    ease:'none',
+    scrollTrigger:{
+      trigger: img,
+      start: "top top",
+      end: "bottom top",
+    
+scrub:true,
+    }
+  },"a1")
+  .to(elem,{
+xPercent:xTransform,
+ease:'none',
+scrollTrigger:{
+  trigger: img,
+  start: "top bottom",
+  end: "bottom top",
+ 
+scrub:true,
+}
+  })
 })
